ba0e16fa85a7bec064668f6604a5a917b2ddc5e7
Kotaro Suzuki <kotarosz727@gmail.com>
2019-01-12 07:11:41 +0000

    css made

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   root 'home#top'
- 
-  get 'home/help' =>"home#help"
-  get 'home/about' => "home#about"
+  get '/help', to:"home#help"
+  get '/about', to:"home#about"
+  get  '/signup',  to: 'users#new'
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
