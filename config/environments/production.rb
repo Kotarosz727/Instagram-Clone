536765dfa2a663f870497282dc717dc9e322fceb
Kotaro Suzuki <kotarosz727@gmail.com>
2019-01-20 07:04:31 +0000

    up date production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,7 +96,7 @@ Rails.application.configure do
     :authentication => :plain,
     :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
+    :domain         => 'fast-brushlands-28336.herokuapp',
     :enable_starttls_auto => true
   }
 
