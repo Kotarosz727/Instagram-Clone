2a093d4ce1f5914ad7cf570688aa8cdf92a319e3
Kotaro Suzuki <kotarosz727@gmail.com>
2019-01-27 03:55:50 +0000

    rewrite carrier wave

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,19 +1,12 @@
-require 'carrierwave/storage/abstract'
-require 'carrierwave/storage/file'
-require 'carrierwave/storage/fog'
-
 if Rails.env.production?
-CarrierWave.configure do |config|
-  config.cache_dir = "#{Rails.root}/tmp/uploads"
-  config.fog_provider = 'fog/aws'
-  config.fog_credentials = {
-
-    provider: 'AWS',
-    region:  ENV['S3_REGION'],
-    aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-
-  }
-  config.fog_directory  = ENV['S3_BUCKET']
-end
+  CarrierWave.configure do |config|
+    config.fog_credentials = {
+      # Amazon S3用の設定
+      :provider              => 'AWS',
+      :region                => ENV['S3_REGION'],     # 例: 'ap-northeast-1'
+      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_key => ENV['S3_SECRET_KEY']
+    }
+    config.fog_directory     =  ENV['S3_BUCKET']
+  end
 end
