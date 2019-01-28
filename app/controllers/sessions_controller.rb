e256c2cf2185a8da11bcf7238306ba4f25be4db4
Kotaro Suzuki <kotarosz727@gmail.com>
2019-01-28 13:08:39 +0000

    makesure  indent and delete no-use file

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,20 +1,19 @@
 class SessionsController < ApplicationController
   
   def new
-    if logged_in?
-    @micropost  = current_user.microposts.build
-    @feed_items = current_user.feed.paginate(page: params[:page]).search(params[:search])
-    @likes = Like.where(micropost_id: params[:micropost_id])
-    end
+　  if logged_in?
+      @micropost  = current_user.microposts.build
+      @feed_items = current_user.feed.paginate(page: params[:page]).search(params[:search])
+      @likes = Like.where(micropost_id: params[:micropost_id])
   end
 
   def create
-  auth = request.env['omniauth.auth']
-  if auth.present?
-    user = User.find_or_create_from_auth(request.env['omniauth.auth'])
-    session[:user_id] = user.id
-    redirect_back_or user
-  else 
+    auth = request.env['omniauth.auth']
+    if auth.present?
+      user = User.find_or_create_from_auth(request.env['omniauth.auth'])
+      session[:user_id] = user.id
+      redirect_back_or user
+    else 
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
@@ -23,12 +22,11 @@ class SessionsController < ApplicationController
       flash.now[:danger] = 'メールアドレスとパスワードの組み合わせは有効ではありません'
       render 'new'
     end
+    end
   end
-end
 
   def destroy
     log_out
     redirect_to root_url
   end
-  
-end
+end  
