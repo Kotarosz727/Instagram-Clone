00a6985af2f2994c51d666d8547897b73d1b84c9
Kotaro Suzuki <kotarosz727@gmail.com>
2019-01-29 12:31:03 +0000

    makesure indent

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,14 +15,14 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_back_or user
     else 
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-      log_in user
-      redirect_back_or user
-    else
-      flash.now[:danger] = 'メールアドレスとパスワードの組み合わせは有効ではありません'
-      render 'new'
-    end
+      user = User.find_by(email: params[:session][:email].downcase)
+      if user && user.authenticate(params[:session][:password])
+        log_in user
+        redirect_back_or user
+      else
+        flash.now[:danger] = 'メールアドレスとパスワードの組み合わせは有効ではありません'
+        render 'new'
+      end
     end
   end
 
